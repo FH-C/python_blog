a5a8f6a9c1d7adad61093d9dd233f39718f40270
CYY666666 <864383740@qq.com>
2019-10-17 16:32:41 +0800

    third commit

--- awesome-python3-webapp/web/app.py
+++ awesome-python3-webapp/web/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
+'''
 import logging; logging.basicConfig(level=logging.INFO)
 import asyncio
 import os
@@ -167,4 +167,4 @@ if __name__ == '__main__':
     app.run()
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
-loop.run_forever()
+loop.run_forever()'''
